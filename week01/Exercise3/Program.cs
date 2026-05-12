8cb6561e0de860bee1362dbaa5d1e966a10664f6
Zen052128 <kzdepaloma@gmail.com>
2026-05-12 23:27:38 +0800

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string notifier = "no";
+        string answer2;
+    
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        do 
+        {
+            Console.Write("What is your guess? ");
+            answer2 = Console.ReadLine();
+        
+            int guess = int.Parse(answer2);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                notifier = "yes";
+                Console.WriteLine("You guessed it right!");
+            }
+
+        } while (!(notifier == "yes"));
     }
 }
